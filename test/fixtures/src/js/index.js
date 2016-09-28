845c5ee9de468ca0bd5dfa80273b872a050312b5
brandonocasey <brandonocasey@gmail.com>
2016-09-28 13:22:46 -0400

    unit tests

---
+++ test/fixtures/src/js/index.js
@@ -0,0 +1,14 @@
+export default class TestingThing {
+  constructor() {
+    this.bar = 'bar';
+  }
+  foo() {
+    return this.bar;
+  }
+}
+
+// intentional double empty line to cause error
+
+
+// long comment to cause a warning in eslint
+// aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
